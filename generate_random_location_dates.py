9e55feaa907d0d5bf5c3fe1829047f59d9491390
dispatchedx <spyros.boukis@outlook.com>
2020-09-11 15:52:04 +0300

    fixes

--- generate_random_location_dates.py
+++ generate_random_location_dates.py
@@ -1,20 +1,24 @@
-import os
+
 import random
-from datetime import datetime
 from random import randrange
-data= '{ "locations" :['
+data = '{ "locations" :['
 
 for i in range(15000):
-    timestamp=random.randint(1420070400000 ,1598358416123 )
-    activities=["WALKING","STILL","ON_BICYCLE","RUNNING","IN_VEHICLE"]
-    activity=activities[randrange(4)]
 
-    data = data + '{ "timestampMs" : "'+str(timestamp)+'","latitudeE7" : 381852128,"longitudeE7" : 217105715,"accuracy" : 64,"activity" : [ {"timestampMs" : "'+str(timestamp)+'","activity" : [ {"type" : "'+activity+'", "confidence" : 100 } ] } ]},' #remove this last traling comma
+    start_timestamp=1420070400000 #2015/8/25
+    end_timestamp=1603843200000 # 2020/10/28
+    #1598358416123 #2020/8/25
+    timestamp=random.randint(start_timestamp ,end_timestamp )
+
+    activities=["WALKING", "STILL", "ON_BICYCLE", "RUNNING", "IN_VEHICLE"]
+    activity=activities[randrange(5)]
+
+    data = data + '{ "timestampMs" : "'+str(timestamp)+'","latitudeE7" : 381852128,"longitudeE7" : 217105715,"accuracy" : 64,"activity" : [ {"timestampMs" : "'+str(timestamp)+'","activity" : [ {"type" : "'+activity+'", "confidence" : 100 } ] } ]},' #removing this last traling comma when it finishes
 
 
-data = data + "]}"
+data = data[:-1] + "]}"
 
-with open('kek.json','w') as file:
+with open('lulw.json', 'w') as file:
     file.write(data)
 
 
